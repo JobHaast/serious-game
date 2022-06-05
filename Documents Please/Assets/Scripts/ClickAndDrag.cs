76437ed0edb7068d3b6600544a4cb904786d435a
Job Haast <jobhaast@hotmail.com>
2022-06-05 23:00:32 +0200

    Movable objects

---
+++ Documents Please/Assets/Scripts/ClickAndDrag.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class ClickAndDrag : MonoBehaviour
+{
+    private bool canMove;
+    private bool dragging;
+    public Collider2D colliderBox;
+
+    void Start()
+    {
+        canMove = false;
+        dragging = false;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (colliderBox == Physics2D.OverlapPoint(mousePos))
+            {
+                canMove = true;
+            }
+            else
+            {
+                canMove = false;
+            }
+            if (canMove)
+            {
+                dragging = true;
+            }
+        }
+        if (dragging)
+        {
+            transform.position = mousePos;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            canMove = false;
+            dragging = false;
+        }
+    }
+}
+
