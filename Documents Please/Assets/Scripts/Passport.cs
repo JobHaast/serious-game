fb4e39bc3b5ca173b7d28022a5eeeff523fcf228
Job Haast <jobhaast@hotmail.com>
2022-06-06 12:15:24 +0200

    Passport template

---
+++ Documents Please/Assets/Scripts/Passport.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+public class Passport : ScriptableObject
+{
+    public string FirstName;
+    public string LastName;
+    public DateTime BirthDate;
+    public Gender Gender;
+    public string Country;
+    public DateTime ExperationDate;
+}
