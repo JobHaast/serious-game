5f7a5c9738093e71c1562382a5994afd3c92dafe
Iza <iza.moerenhout@hotmail.com>
2022-06-10 17:05:36 +0200

    Opzet scorescreen

---
+++ Documents Please/Assets/Scripts/Score.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    //public int savings;
+
+    public GameObject salaryText;
+    public int salary;
+
+    //public int rent;
+    //public int food;
+    //public int heat;
+
+}
